fea66ef72fc5d5b70e2fd979a404ea0afecedf5c
Danil Kolotushkin <dkolon@gmail.com>
2026-03-06 17:39:04 +0200

    Kamailio config and changes to local client

--- run-local-client.sh
+++ run-local-client.sh
@@ -5,9 +5,10 @@
 set -e
 cd "$(dirname "$0")"
 
-export SCAIP_TRANSPORT=udp
 export SCAIP_SERVER_HOST=127.0.0.1
 export SCAIP_SERVER_PORT=5060
 export SCAIP_CLIENT_HOST=127.0.0.1
+export SCAIP_CLIENT_PORT=5070
+export SCAIP_TRANSPORT=udp
 
-mvn exec:java@run-client -q
+mvn exec:java@run-client
